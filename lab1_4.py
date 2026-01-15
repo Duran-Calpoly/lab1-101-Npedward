6801d6e5c2aa5deaf5151d655a8901fc8d346c32
Npedward <npedward@calpoly.edu>
2026-01-14 18:33:28 -0800

    Update lab1_4.py

--- lab1_4.py
+++ lab1_4.py
@@ -2,13 +2,16 @@ def calculate_average(num1, num2, num3):
   return(num1 + num2 + num3) / 3
 
 average = calculate_average(num1,num2,num3)
+print(average)
 
 def add_tax(bill_total):
   return(bill_total) * 1.1
 
 tax = add_tax(bill_total)
+print(tax)
 
 def greet_user(name)
   return("Hello, " + name)
 
 greeting = greet_user(name)
+print(greeting)
