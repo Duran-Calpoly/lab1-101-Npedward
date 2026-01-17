057b8981864224e2850683404833e7f9424be1dd
Npedward <npedward@calpoly.edu>
2026-01-17 03:29:50 +0000

    Lab 2 Done

--- lab1_5.py
+++ lab1_5.py
@@ -1,1 +1,18 @@
-pass
+def check_multiple(number):
+    return number % 3 == 0 and number % 5 == 0
+
+def check_password(input_string):
+    if input_string == ("Python123"):
+        return ("access granted")
+    else:
+        return ("access denied")
+
+def calculate_federal_tax(salary):
+    if salary <= 11000:
+        return (salary * .1)
+    elif salary <= 44725:
+        return (salary * .12)
+    elif salary <= 95375:
+        return (salary * .22)
+    else:
+        return (salary * .24)
